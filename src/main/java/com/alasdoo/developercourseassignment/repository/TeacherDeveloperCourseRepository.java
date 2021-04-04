4c1186ec4010db397db92ad3ab9706d85042fb76
markomarinkovic <markomarinkovic@192.168.1.2>
2021-04-04 18:36:46 +0200

    Add findByDeveloperCourseId in TeacherDeveloperCourseRepo

--- src/main/java/com/alasdoo/developercourseassignment/repository/TeacherDeveloperCourseRepository.java
+++ src/main/java/com/alasdoo/developercourseassignment/repository/TeacherDeveloperCourseRepository.java
@@ -4,6 +4,7 @@ import com.alasdoo.developercourseassignment.entity.TeacherDeveloperCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface TeacherDeveloperCourseRepository extends JpaRepository<TeacherD
     Optional<TeacherDeveloperCourse> findByDeveloperCourseIdAndTeacherId(Integer developerCourseId, Integer teacherId);
 
     Optional<TeacherDeveloperCourse> findByTeacherId(Integer teacherId);
+    
+    Optional<List<TeacherDeveloperCourse>> findByDeveloperCourseId(Integer developerCourseId);
 }
